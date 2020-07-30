d0b0d971a07af58dbb5898ece24c2c11f57f0359
zaj-e <carbonelmarcelo@gmail.com>
2020-07-29 20:07:55 -0500

    Remove NSInclude because it's based on reflection (slow)

--- Reservation-Service/routers/router.go
+++ Reservation-Service/routers/router.go
@@ -3,10 +3,17 @@ package routers
 import (
 	"Reservation-Service/controllers"
 	"github.com/astaxie/beego"
+	"github.com/astaxie/beego/context"
 )
 
 func init() {
 	ns := beego.NewNamespace("/api",
+		beego.NSCond(func(ctx *context.Context) bool {
+    	    if ctx.Input.Domain() == "localhost" {
+    	        return true
+    	    }
+    	    return false
+    	}),
 		beego.NSNamespace("/users",
 			beego.NSInclude(
 				&controllers.UserController{},
